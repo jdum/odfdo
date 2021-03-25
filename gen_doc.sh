777c1a53fa2682e78cb19b7c7852825938b5e14c
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2021-03-25 01:38:33 +0100

    some hint in gen_doc.sh to find pdoc software

--- gen_doc.sh
+++ gen_doc.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+#
+# if pdoc is missing: pip install pdoc3
+#
+
 [ -d doc.old ] && rm -r doc.old
 [ -d doc ] && mv doc doc.old
-pdoc -f --html odfdo 
+pdoc -f --html odfdo
 mv html/odfdo doc
 mv html doc.old
 cp odfdo.png doc
-
