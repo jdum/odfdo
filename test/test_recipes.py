7f0c19313607c470dd0b31ffc7b44fc5a4a91cce
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2023-10-25 19:05:22 +0200

    add test_recipes.py

---
+++ test/test_recipes.py
@@ -0,0 +1,26 @@
+import pytest
+import subprocess as sp
+from pathlib import Path
+
+RECIPES = Path(__file__).parent.parent / "recipes"
+
+
+def list_recipes() -> list[Path]:
+    return list(RECIPES.glob("*.py"))
+
+
+@pytest.mark.parametrize("recipe", list_recipes())
+def test_recipe(recipe):
+    print(recipe)
+    try:
+        sp.run(
+            ["python", recipe],
+            stdout=sp.PIPE,
+            stderr=sp.STDOUT,
+            timeout=60,
+            check=True,
+            text=True,
+        )
+    except sp.CalledProcessError as e:
+        print(e.stdout)
+        raise
