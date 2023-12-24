56ad32a768a5a50796b5c6bf294a5729bbddcc3e
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2023-12-24 20:01:01 +0100

    add recipes for save/read document from io.BytesIO

---
+++ recipes/read_document_from_bytesio.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Read a document from BytesIO.
+"""
+import io
+from pathlib import Path
+
+from odfdo import Document
+
+DATA = Path(__file__).parent / "data"
+SOURCE = "lorem.odt"
+
+
+def main():
+    file_path = DATA / SOURCE
+    with io.BytesIO() as bytes_content:
+        # read the file in the BytesIO (or read from some network)
+        bytes_content.write(file_path.read_bytes())
+        # Create the odfdo.Document from the BytesIO
+        bytes_content.seek(0)
+        document = Document(bytes_content)
+        # check :
+        if document.body.search("Lorem ipsum dolor sit amet") is None:
+            raise ValueError("string not found")
+
+
+if __name__ == "__main__":
+    main()
---
+++ recipes/save_document_as_bytesio.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""Save a document as BytesIO.
+"""
+import io
+from pathlib import Path
+
+from odfdo import Document, Paragraph
+
+OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "bytes"
+TARGET = "document.odt"
+
+
+def make_document():
+    document = Document("text")
+    body = document.body
+    paragraph = Paragraph("Hello World")
+    body.append(paragraph)
+    return document
+
+
+def main():
+    document = make_document()
+    with io.BytesIO() as bytes_content:
+        document.save(bytes_content)
+        # Now use the BytesIO in some way:
+        # In a netwotk context, typically:
+        #    response.write(bytes_content.getvalue())
+        OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+        with open(OUTPUT_DIR / TARGET, "wb") as file:
+            file.write(bytes_content.getvalue())
+
+
+if __name__ == "__main__":
+    main()
