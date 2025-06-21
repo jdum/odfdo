bbacd7fb479f4ed5a77c9aac3741446651e40fdc
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-06-21 11:22:40 +0200

    refactor recipe accessing_a_list_of_elements.py

--- recipes/accessing_a_list_of_elements.py
+++ recipes/accessing_a_list_of_elements.py
@@ -11,6 +11,18 @@ These methods or properties return a list of elements:
     - `body.tables`
     - `body.get_paragraphs(content)`
 """
+
+# Expected result on stdout:
+# 96 get methods are available
+# number of headings: 29
+# number of images stored: 0
+# number of paragraphs: 175
+# number of links (URLs): 352
+# number of footnotes: 49
+# number of tables: 0
+# Paragraphs with 'Fish': 4
+# Paragraphs with 'answer' and '42': 1
+
 import os
 import sys
 from pathlib import Path
@@ -22,7 +34,8 @@ DATA = Path(__file__).parent / "data"
 SOURCE = "collection2.odt"
 
 
-def read_source_document():
+def read_source_document() -> Document:
+    """Return the source Document."""
     try:
         source = sys.argv[1]
     except IndexError:
@@ -30,8 +43,19 @@ def read_source_document():
     return Document(source)
 
 
-def main():
-    document = read_source_document()
+def analysis(document: Document) -> dict[str, int]:
+    """Returns some statistics about the document."""
+    result: dict[str, int] = {
+        "methods": 0,
+        "headings": 0,
+        "images": 0,
+        "paragraphs": 0,
+        "links": 0,
+        "footnotes": 0,
+        "tables": 0,
+        "fish": 0,
+        "answer": 0,
+    }
 
     # The body object is an XML element from which we can access one or several
     # other elements we are looking for.
@@ -40,58 +64,63 @@ def main():
     # Accessing a list of elements
     # Should you need to access all elements of a kind, there are the
     # get_xxxs methods, where xxx can be paragraph, heading, list, table, ...
-    count_methods = " ".join(dir(body)).count("get_")
-    print(f"{count_methods} get methods are available")
+    # Methods without parameters are accessible through properties.
+    result["methods"] = " ".join(dir(body)).count("get_")
     # Some examples, that you can check against actual content of the odt file:
     # See how complex is our wikipedia documents:
-    print("number of headings:", len(body.headers))
-    print("number of images stored:", len(body.images))
-    print("number of paragraphs:", len(body.paragraphs))
-    print("number of links (URLs):", len(body.get_links()))
-    print("number of footnotes:", len(body.get_notes()))
+    result["headings"] = len(body.headers)
+    result["images"] = len(body.images)
+    result["paragraphs"] = len(body.paragraphs)
+    result["links"] = len(body.get_links())
+    result["footnotes"] = len(body.get_notes())
     # Our sample document has no table:
     # print("number of tables:", len(body.get_tables()))
-    print("number of tables:", len(body.tables))
+    result["tables"] = len(body.tables)
 
     # Each get_xxx_list method provides parameters for filtering the results.
     # For example headings can be listed by level, annotations by creator, etc.
     # Almost all of them accept filtering by style and content using a regular
     # expressions.
-    print("Paragraphs with 'Fish':", len(body.get_paragraphs(content=r"Fish")))
-    print(
-        "Paragraphs with 'answer' and '42':",
-        len(body.get_paragraphs(content=r"answer.*42")),
-    )
+    result["fish"] = len(body.get_paragraphs(content=r"Fish"))
+    result["answer"] = len(body.get_paragraphs(content=r"answer.*42"))
 
-    _expected_result = """
-    96 get methods are available
-    number of headings: 29
-    number of images stored: 0
-    number of paragraphs: 175
-    number of links (URLs): 352
-    number of footnotes: 49
-    number of tables: 0
-    Paragraphs with 'Fish': 4
-    Paragraphs with 'answer' and '42': 1
-    """
-
-    test_unit(document)
+    return result
 
 
-def test_unit(document: Document) -> None:
+def display_analysis(stats: dict[str, int]) -> None:
+    """Print the stats on stdout."""
+    print(f"{stats['methods']} get methods are available")
+    print(f"number of headings: {stats['headings']}")
+    print(f"number of images stored: {stats['images']}")
+    print(f"number of paragraphs: {stats['paragraphs']}")
+    print(f"number of links (URLs): {stats['links']}")
+    print(f"number of footnotes: {stats['footnotes']}")
+    print(f"number of tables: {stats['tables']}")
+    print(f"Paragraphs with 'Fish': {stats['fish']}")
+    print(f"Paragraphs with 'answer' and '42': {stats['answer']}")
+
+
+def main() -> None:
+    document = read_source_document()
+    stats = analysis(document)
+    display_analysis(stats)
+    test_unit(stats)
+
+
+def test_unit(stats: dict[str, int]) -> None:
     # only for test suite:
     if "ODFDO_TESTING" not in os.environ:
         return
-    body = document.body
-    count_methods = " ".join(dir(body)).count("get_")
-    assert count_methods == 96
-    assert len(body.headers) == 29
-    assert len(body.images) == 0
-    assert len(body.paragraphs) == 175
-    assert len(body.get_links()) == 352
-    assert len(body.get_notes()) == 49
-    assert len(body.get_paragraphs(content=r"Fish")) == 4
-    assert len(body.get_paragraphs(content=r"answer.*42")) == 1
+
+    assert stats["methods"] == 96
+    assert stats["headings"] == 29
+    assert stats["images"] == 0
+    assert stats["paragraphs"] == 175
+    assert stats["links"] == 352
+    assert stats["footnotes"] == 49
+    assert stats["tables"] == 0
+    assert stats["fish"] == 4
+    assert stats["answer"] == 1
 
 
 if __name__ == "__main__":
