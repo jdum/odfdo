c378b1f3a01761b4d97fc1f38fd878b44b34eedd
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-06-21 13:34:57 +0200

    refactor recipe how_to_add_a_table_to_a_document.py

--- recipes/how_to_add_a_table_to_a_document.py
+++ recipes/how_to_add_a_table_to_a_document.py
@@ -1,11 +1,14 @@
-"""Minimal example of how to add a table to a text document.
-"""
+"""Minimal example of how to add a table to a text document."""
+
+import os
+
 from odfdo import Document, Header, Paragraph, Table
 
 _DOC_SEQUENCE = 55
 
 
-def main():
+def generate_document() -> Document:
+    """Add a 3x3 table to a new document."""
     document = Document("text")
     body = document.body
 
@@ -16,6 +19,21 @@ def main():
     # Creating a table :
     table = Table("Table 1", width=3, height=3)
     body.append(table)
+    return document
+
+
+def main() -> None:
+    document = generate_document()
+    test_unit(document)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    table = document.body.get_table(0)
+    assert table.size == (3, 3)
 
 
 if __name__ == "__main__":
