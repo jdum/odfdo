dee8555243baf6657cda713ef18fd913869fb259
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-06-21 13:47:34 +0200

    refactor recipe make_a_presentation_from_text_with_different_styles.py

--- recipes/make_a_presentation_from_text_with_different_styles.py
+++ recipes/make_a_presentation_from_text_with_different_styles.py
@@ -2,14 +2,16 @@
 """Each line of the text becomes a slide of the presentation, we change of style
 depending on the length of text line.
 """
+
+import os
 from pathlib import Path
 
 from odfdo import Document, DrawPage, Frame, Style
 
 _DOC_SEQUENCE = 287
-# lst = open(sys.argv[1]).readlines()
 OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "styled_prez"
 TARGET = "presentation.odp"
+
 CONTENT = """123
 azertyuiop
 azertyuiop azertyuiop
@@ -24,24 +26,16 @@ end.
 """.splitlines()
 
 
-def save_new(document: Document, name: str):
+def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     new_path = OUTPUT_DIR / name
     print("Saving:", new_path)
     document.save(new_path, pretty=True)
 
 
-def main():
-    document = generate_document()
-    save_new(document, TARGET)
-
-
-def generate_document():
-    presentation = Document("presentation")
-    body = presentation.body
-    body.clear()
-
-    # Creating a smooth style for the graphic item
+def create_style() -> Style:
+    """Creating a smooth style for the graphic item."""
     base_style = Style(
         "graphic",
         name="Gloup48",
@@ -69,7 +63,16 @@ def generate_document():
         size="48pt",
         weight="bold",
     )
+    return base_style
 
+
+def generate_document() -> Document:
+    """Generate a Presentation Document with different styles."""
+    presentation = Document("presentation")
+    body = presentation.body
+    body.clear()
+
+    base_style = create_style()
     presentation.insert_style(base_style)
 
     # Making o lot of variations
@@ -83,7 +86,7 @@ def generate_document():
 
     for count, blurb in enumerate(CONTENT):
         text = blurb
-        name = f"{count + 1} - { text[:10]}"
+        name = f"{count + 1} - {text[:10]}"
         page = DrawPage(name)
         # choosing some style:
         size = 48
@@ -104,6 +107,26 @@ def generate_document():
         body.append(page)
 
     return presentation
+
+
+def main() -> None:
+    document = generate_document()
+    test_unit(document)
+    save_new(document, TARGET)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    body = document.body
+    count = len([item for item in body.children if isinstance(item, DrawPage)])
+    assert count == len(CONTENT)
+    first_page = body.children[0]
+    assert str(first_page).strip() == CONTENT[0].strip()
+    last_page = body.children[-1]
+    assert str(last_page).strip() == CONTENT[-1].strip()
 
 
 if __name__ == "__main__":
