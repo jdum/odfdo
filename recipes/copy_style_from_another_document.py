826c0bb075ac13177a5cf3d37e88eb28879ed193
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-06-28 10:34:18 +0200

    refactor recipe copy_style_from_another_document.py

--- recipes/copy_style_from_another_document.py
+++ recipes/copy_style_from_another_document.py
@@ -3,6 +3,8 @@
 
 For more advanced version, see the odfdo-style script.
 """
+
+import os
 import sys
 from pathlib import Path
 
@@ -17,7 +19,8 @@ OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "styled1"
 TARGET = "document.odt"
 
 
-def read_source_document():
+def read_source_document() -> Document:
+    """Return the source Document."""
     try:
         source = sys.argv[1]
     except IndexError:
@@ -25,21 +28,19 @@ def read_source_document():
     return Document(source)
 
 
-def save_new(document: Document, name: str):
+def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     new_path = OUTPUT_DIR / name
     print("Saving:", new_path)
     document.save(new_path, pretty=True)
 
 
-def main():
+def merge_styles(document: Document) -> None:
     # We want to change the styles of collection2.odt,
     # we know the odfdo_styles.odt document contains an interesting style,
     # So let's first fetch the style:
     style_document = Document(STYLE_SOURCE)
-
-    # Open our document:
-    document = read_source_document()
 
     # We could change only some styles, but here we want a clean basis:
     document.delete_styles()
@@ -47,7 +48,20 @@ def main():
     # And now the actual style change:
     document.merge_styles_from(style_document)
 
+
+def main() -> None:
+    document = read_source_document()
+    merge_styles(document)
+    test_unit(document)
     save_new(document, TARGET)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    assert len(document.get_styles()) == 75
 
 
 if __name__ == "__main__":
