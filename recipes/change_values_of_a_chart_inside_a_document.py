8a88c24c832e25361bf18ea1ca61088a66c9c8ef
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-06-28 10:25:05 +0200

    refactor recipe change_values_of_a_chart_inside_a_document.py

--- recipes/change_values_of_a_chart_inside_a_document.py
+++ recipes/change_values_of_a_chart_inside_a_document.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-"""Open a text document with an embedded chart and change some values.
-"""
+"""Open a text document with an embedded chart and change some values."""
 
+import os
+import sys
 from pathlib import Path
 
 # for cell style
@@ -9,25 +10,30 @@ from odfdo import Document
 
 _DOC_SEQUENCE = 295
 DATA = Path(__file__).parent / "data"
-SOURCE = DATA / "chart.odt"
+SOURCE = "chart.odt"
 OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "modified_chart"
 TARGET = "modified_chart.odt"
 
 
+def read_source_document() -> Document:
+    """Return the source Document."""
+    try:
+        source = sys.argv[1]
+    except IndexError:
+        source = DATA / SOURCE
+    return Document(source)
+
+
 def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     new_path = OUTPUT_DIR / name
     print("Saving:", new_path)
     document.save(new_path, pretty=True)
 
 
-def main() -> None:
-    document = Document(SOURCE)
-    change(document)
-    save_new(document, TARGET)
-
-
 def change(document: Document) -> None:
+    """Change some values in the embedded chart table."""
     # list the parts if needed
     print(document.parts)
     # -> ['mimetype', 'ObjectReplacements/Object 1', 'Object 1/meta.xml', 'Object 1/styles.xml', 'Object 1/content.xml', ...
@@ -51,6 +57,26 @@ def change(document: Document) -> None:
     table.set_value("A2", "label changed")
     table.set_value("D3", 4000)
     table.set_value("D4", 4321)
+
+
+def main() -> None:
+    document = read_source_document()
+    change(document)
+    test_unit(document)
+    save_new(document, TARGET)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    part = document.get_part("Object 1/content.xml")
+    table = part.body.get_table(0)
+    assert table.get_value("A3") == "Row 2"
+    assert table.get_value("A2") == "label changed"
+    assert table.get_value("D3") == 4000
+    assert table.get_value("D4") == 4321
 
 
 if __name__ == "__main__":
