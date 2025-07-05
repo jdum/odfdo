8f41803808c248fcf8f9b8fcf621a0bacdbe31ee
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 12:11:25 +0200

    refactor recipe how_to_add_an_item_to_a_list

--- recipes/how_to_add_an_item_to_a_list.py
+++ recipes/how_to_add_an_item_to_a_list.py
@@ -1,15 +1,33 @@
-"""Minimal example of how to add an item to a list.
-"""
+#!/usr/bin/env python
+"""Minimal example of how to add an item to a list."""
+
+import os
 
 from odfdo import List, ListItem
 
 _DOC_SEQUENCE = 28
 
 
-def main():
-    a_list = List(["chocolat", "café"])
-    item = ListItem("thé")
-    a_list.append(item)
+def generate_list() -> List:
+    """Return a document with a 3x3 table."""
+    drink_list = List(["chocolate", "coffee"])
+    item = ListItem("tea")
+    drink_list.append(item)
+
+    return drink_list
+
+
+def main() -> None:
+    some_list = generate_list()
+    test_unit(some_list)
+
+
+def test_unit(some_list: List) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    assert str(some_list).strip() == "-  chocolate\n -  coffee\n -  tea"
 
 
 if __name__ == "__main__":
