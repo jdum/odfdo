8fa433b5703804ec6f340d085e35ccdd950af178
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 09:45:11 +0200

    refactor recipe create_a_basic_text_document_with_a_list

--- recipes/create_a_basic_text_document_with_a_list.py
+++ recipes/create_a_basic_text_document_with_a_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-"""Create a basic text document with a list.
-"""
+"""Create a basic text document with a list."""
+
 import os
 from pathlib import Path
 
@@ -11,20 +11,16 @@ OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "basic_list"
 TARGET = "document.odt"
 
 
-def save_new(document: Document, name: str):
+def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     new_path = OUTPUT_DIR / name
     print("Saving:", new_path)
     document.save(new_path, pretty=True)
 
 
-def main():
-    document = generate_document()
-    test_unit(document)
-    save_new(document, TARGET)
-
-
-def generate_document():
+def generate_document() -> Document:
+    """Generate a basic text document with a list."""
     # Create the document
     document = Document("text")
     body = document.body
@@ -50,10 +46,17 @@ def generate_document():
     return document
 
 
+def main() -> None:
+    document = generate_document()
+    test_unit(document)
+    save_new(document, TARGET)
+
+
 def test_unit(document: Document) -> None:
     # only for test suite:
     if "ODFDO_TESTING" not in os.environ:
         return
+
     assert str(document).strip() == "- Arthur\n- Ford\n- Trillian\n- Marvin"
 
 
