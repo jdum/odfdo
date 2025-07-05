5003c9f38a5eae864223919e833777d6b5c4f669
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 09:30:26 +0200

    refactor recipe extract_a_sub_table_from_some_big_table

--- recipes/extract_a_sub_table_from_some_big_table.py
+++ recipes/extract_a_sub_table_from_some_big_table.py
@@ -1,60 +1,50 @@
 #!/usr/bin/env python
-"""Create a table of 1000 lines and 100 columns, extract a sub table
+"""Open a table of 1000 lines and 100 columns, extract a sub table
 of 100 lines 26 columns, save the result in a spreadsheet document.
 """
+
 import os
+import sys
 from pathlib import Path
 
 from odfdo import Document, Row, Table
 
 _DOC_SEQUENCE = 450
+DATA = Path(__file__).parent / "data"
+SOURCE = "big_table.ods"
 OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "extract_table"
 TARGET = "document.ods"
 
 
-def save_new(document: Document, name: str):
+def read_source_document() -> Document:
+    """Return the source Document."""
+    try:
+        source = sys.argv[1]
+    except IndexError:
+        source = DATA / SOURCE
+    return Document(source)
+
+
+def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     new_path = OUTPUT_DIR / name
     print("Saving:", new_path)
     document.save(new_path, pretty=True)
 
 
-def syracuse(n: int) -> int:
-    if n % 2 == 0:
-        return n // 2
-    return 3 * n + 1
-
-
-def generate_big_table(table_name) -> Document:
-    spreadsheet = Document("spreadsheet")
-    body = spreadsheet.body
-    body.clear()
-    table = Table(table_name)
-    body.append(table)
-
-    lines = 1000
-    cols = 100
-
-    for line in range(lines):
-        row = Row()
-        values = []
-        n = line
-        for _i in range(cols):
-            values.append(n)
-            n = syracuse(n)
-        row.set_values(values)
-        table.append(row)
-
-    return spreadsheet
-
-
-def main():
-    table_name = "Big Table"
-    spreadsheet = generate_big_table(table_name)
-    body = spreadsheet.body
-    big_table = body.get_table(name=table_name)
+def extract_sub_tables(document: Document) -> Document:
+    """Return a spreadsheet with 2 sheets extracted from the imput document."""
+    # Expected_result:
+    #  Size of Big Table : (100, 1000)
+    #  Size of extracted table 1 : (26, 100)
+    #  Size of extracted table 2 : (26, 100)
+    #
+    big_table = document.body.get_table(name="Big Table")
     print("Size of Big Table :", big_table.size)
 
+    extracted = Document("ods")
+    extracted.body.clear()
     # now extract 100 rows of 26 columns :
     table1 = Table("Extract 1")
     for r in range(800, 900):
@@ -63,34 +53,34 @@ def main():
         new_row = Row()
         new_row.set_values(extracted_values)
         table1.append(new_row)
-    body.append(table1)
+    extracted.body.append(table1)
     print("Size of extracted table 1 :", table1.size)
 
     # other method
     table2 = Table("Extract 2")
     cells = big_table.get_cells(coord=(50, 800, 75, 899))
     table2.set_cells(coord=(0, 0), cells=cells)
-    body.append(table2)
+    extracted.body.append(table2)
     print("Size of extracted table 2 :", table2.size)
 
-    test_unit(spreadsheet)
+    return extracted
 
-    save_new(spreadsheet, TARGET)
 
-    _expected_result = """
-Size of Big Table : (100, 1000)
-Size of extracted table 1 : (26, 100)
-Size of extracted table 2 : (26, 100)
-"""
+def main() -> None:
+    document = read_source_document()
+    extracted = extract_sub_tables(document)
+    test_unit(extracted)
+    save_new(extracted, TARGET)
 
 
 def test_unit(spreadsheet: Document) -> None:
     # only for test suite:
     if "ODFDO_TESTING" not in os.environ:
         return
+
     body = spreadsheet.body
     table1 = body.get_table(position=0)
-    assert table1.size == (100, 1000)
+    assert table1.size == (26, 100)
     table2 = body.get_table(position=1)
     assert table2.size == (26, 100)
 
