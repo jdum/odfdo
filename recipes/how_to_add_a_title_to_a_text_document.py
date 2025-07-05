d60d8fb24f8cbe6935d78658a56825ac712b2847
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 12:03:25 +0200

    refactor recipe how_to_add_a_title_to_a_text_document

--- recipes/how_to_add_a_title_to_a_text_document.py
+++ recipes/how_to_add_a_title_to_a_text_document.py
@@ -1,17 +1,35 @@
-"""Minimal example of how to add a Header of first level to a text document.
-"""
+#!/usr/bin/env python
+"""Minimal example of how to add a Header of first level to a text document."""
+
+import os
 
 from odfdo import Document, Header
 
 _DOC_SEQUENCE = 67
 
 
-def main():
+def generate_document() -> Document:
+    """Return a document with a title."""
     document = Document("text")
-    body = document.body
 
     title1 = Header(1, "The Title")
-    body.append(title1)
+    document.body.append(title1)
+
+    return document
+
+
+def main() -> None:
+    document = generate_document()
+    test_unit(document)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    title = document.body.get_header(0)
+    assert str(title).strip() == "The Title"
 
 
 if __name__ == "__main__":
