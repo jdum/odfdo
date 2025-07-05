38ecbebe182ce5abe6ae03bc166895f63b579763
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 11:39:19 +0200

    refactor recipe how_to_add_a_sublist_to_a_list

--- recipes/how_to_add_a_sublist_to_a_list.py
+++ recipes/how_to_add_a_sublist_to_a_list.py
@@ -1,12 +1,26 @@
-"""Minimal example of how to add a paragraph.
-"""
+#!/usr/bin/env python
+"""Minimal example of how to add a sublist to a list."""
+
+import os
+from pathlib import Path
 
 from odfdo import Document, List, ListItem
 
 _DOC_SEQUENCE = 27
+OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "minimal_list"
+TARGET = "document.odt"
 
 
-def main():
+def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    new_path = OUTPUT_DIR / name
+    print("Saving:", new_path)
+    document.save(new_path, pretty=True)
+
+
+def generate_document() -> Document:
+    """Return a text document with a list."""
     document = Document("text")
     body = document.body
 
@@ -20,6 +34,22 @@ def main():
     item.append(List(["thé vert", "thé rouge"]))
 
     print(body.serialize(True))
+
+    return document
+
+
+def main() -> None:
+    document = generate_document()
+    test_unit(document)
+    save_new(document, TARGET)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    assert len(document.body.get_lists()) == 2
 
 
 if __name__ == "__main__":
