572273d15b9fc81a23d6ea88a9025b7cfeb7b9b3
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 13:07:45 +0200

    refactor recipe how_to_write_hello_world_in_a_text_document

--- recipes/how_to_write_hello_world_in_a_text_document.py
+++ recipes/how_to_write_hello_world_in_a_text_document.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-"""Create a basic spreadsheet with "Hello World" in the first cell.
-"""
+"""Create a minimal text document with "Hello World" in a pragraph."""
+
 import os
 from pathlib import Path
 
@@ -11,20 +11,26 @@ OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "basic_hello"
 TARGET = "document.odt"
 
 
-def save_new(document: Document, name: str):
+def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     new_path = OUTPUT_DIR / name
     print("Saving:", new_path)
     document.save(new_path, pretty=True)
 
 
-def main():
+def generate_document() -> Document:
     document = Document("text")
     body = document.body
     body.clear()
     paragraph = Paragraph("Hello World")
     body.append(paragraph)
 
+    return document
+
+
+def main() -> None:
+    document = generate_document()
     test_unit(document)
     save_new(document, TARGET)
 
@@ -33,8 +39,8 @@ def test_unit(document: Document) -> None:
     # only for test suite:
     if "ODFDO_TESTING" not in os.environ:
         return
+
     text = str(document.body)
-    print(text)
     assert text == "Hello World\n"
 
 
