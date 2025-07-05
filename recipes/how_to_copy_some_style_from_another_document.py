05002f017c9504e91e07df9a1c91a146675b8a45
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 12:33:24 +0200

    refactor recipe how_to_copy_some_style_from_another_document

--- recipes/how_to_copy_some_style_from_another_document.py
+++ recipes/how_to_copy_some_style_from_another_document.py
@@ -1,40 +1,53 @@
+#!/usr/bin/env python
 """Minimal example of copy of a style from another document.
+
+Document.get_style() main parameters:
+family        : The family of the style, text styles apply on individual
+                characters.
+display_name  : The name of the style as we see it in a desktop
+                application. Styles have an internal name
+                (“Yellow_20_Highlight” in this example) but here we use
+                the display_name instead.
 """
 
-from odfdo import Document, Style
+import os
+from pathlib import Path
+
+from odfdo import Document
 
 _DOC_SEQUENCE = 310
+DATA = Path(__file__).parent / "data"
+SOURCE = "lpod_styles.odt"
 
 
-def main():
+def generate_document() -> Document:
+    """Return a document with a style read from another document."""
     document = Document("text")
     body = document.body
     body.clear()
 
-    # Let's imagine the sample_styles.odt document contains an interesting style.
-    #
-    # So let's first fetch the style:
-    try:
-        odfdo_styles = Document("sample_styles.odt")
-        highlight = odfdo_styles.get_style("text", display_name="Yellow Highlight")
-    except Exception:
-        # let's create some *very simple* text style.
-        highlight = Style(
-            "text", display_name="Yellow Highlight", color="blue", italic=True
-        )
+    styled_source = Document(DATA / SOURCE)
+    highlight_style = styled_source.get_style(
+        family="text", display_name="Yellow Highlight"
+    )
 
-    # We made some assumptions here:
-    #
-    # 'text'              : The family of the style, text styles apply on
-    #                       individual characters.
-    # ”Yellow Highlight”  : The name of the style as we see it in a desktop
-    #                       application.
-    # display_name        : Styles have an internal name (“Yellow_20_Highlight”
-    #                       in this example) but we gave the display_name
-    #                       instead.
-    #
-    # We hopefully have a style object that we add to our own collection:
-    document.insert_style(highlight, automatic=True)
+    document.insert_style(highlight_style, automatic=True)
+
+    return document
+
+
+def main() -> None:
+    document = generate_document()
+    test_unit(document)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    style = document.get_style(family="text", display_name="Yellow Highlight")
+    assert style.display_name == "Yellow Highlight"
 
 
 if __name__ == "__main__":
