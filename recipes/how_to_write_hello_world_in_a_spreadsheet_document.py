5c29096976fcd0de97c3e680238183c80b2baa8a
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 12:56:12 +0200

    refactor recipe how_to_write_hello_world_in_a_spreadsheet_document

--- recipes/how_to_write_hello_world_in_a_spreadsheet_document.py
+++ recipes/how_to_write_hello_world_in_a_spreadsheet_document.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-"""Create a basic spreadsheet with "Hello World" in the first cell.
-"""
+"""Create a minimal spreadsheet with "Hello World" in the first cell."""
+
 import os
 from pathlib import Path
 
@@ -11,21 +11,28 @@ OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "basic_ods"
 TARGET = "document.ods"
 
 
-def save_new(document: Document, name: str):
+def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     new_path = OUTPUT_DIR / name
     print("Saving:", new_path)
     document.save(new_path, pretty=True)
 
 
-def main():
+def generate_document() -> Document:
     document = Document("spreadsheet")
     body = document.body
     body.clear()
 
-    table = Table("Empty Table")
+    table = Table("Hello table")
     table.set_value("A1", "Hello World")
     body.append(table)
+
+    return document
+
+
+def main() -> None:
+    document = generate_document()
     test_unit(document)
     save_new(document, TARGET)
 
@@ -34,6 +41,7 @@ def test_unit(document: Document) -> None:
     # only for test suite:
     if "ODFDO_TESTING" not in os.environ:
         return
+
     text = document.body.get_table(0).get_cell((0, 0)).value.strip()
     print(text)
     assert text == "Hello World"
