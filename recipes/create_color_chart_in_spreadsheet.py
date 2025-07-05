7e4dff4f1e674a86c15352cd0d1d6c79bede3218
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-05 10:25:42 +0200

    refactor recipe create_color_chart_in_spreadsheet

--- recipes/create_color_chart_in_spreadsheet.py
+++ recipes/create_color_chart_in_spreadsheet.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
-"""Create some color chart in a spreadsheet using cells styles.
-(adapted from the odfdo library test cases)
+"""Create some color chart in a spreadsheet using cells styles functions.
+
+For cells, use of functions:
+    make_table_cell_border_string()
+    create_table_cell_style()
+    rgb2hex()
+
+Apply a row style to define the row height.
+
+Apply a column style to define the column width.
 """
 
+import os
 from pathlib import Path
 
 from odfdo import (
@@ -11,7 +20,6 @@ from odfdo import (
     Row,
     Style,
     Table,
-    __version__,
     create_table_cell_style,
     make_table_cell_border_string,
     rgb2hex,
@@ -22,22 +30,26 @@ OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "chart"
 TARGET = "color_chart.ods"
 
 
-def save_new(document: Document, name: str):
+def save_new(document: Document, name: str) -> None:
+    """Save a recipe result Document."""
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
     new_path = OUTPUT_DIR / name
     print("Saving:", new_path)
-    document.save(new_path)
+    document.save(new_path, pretty=True)
 
 
-def hello_messages():
-    print("odfdo installation test")
-    print(f" Version           : {__version__}")
-    print()
-    print(f"Generating color chart in {TARGET}")
-    print("...")
+def generate_chart() -> Document:
+    """Generate a spreadsheet with colored cells.
 
+    For cells, use of functions:
+        make_table_cell_border_string()
+        create_table_cell_style()
+        rgb2hex()
 
-def generate_chart():
+    Apply a row style to define the row height.
+
+    Apply a column style to define the column width.
+    """
     document = Document("spreadsheet")
     body = document.body
     body.clear()
@@ -47,18 +59,21 @@ def generate_chart():
         row = Row()
         for x in range(0, 256, 32):
             cell_value = (x, y, (x + y) % 256)
-            border_rl = make_table_cell_border_string(thick="0.20cm", color="white")
-            border_bt = make_table_cell_border_string(
+            border_right_left = make_table_cell_border_string(
+                thick="0.20cm",
+                color="white",
+            )
+            border_top_bottom = make_table_cell_border_string(
                 thick="0.80cm",
                 color="white",
             )
             style = create_table_cell_style(
                 color="grey",
                 background_color=cell_value,
-                border_right=border_rl,
-                border_left=border_rl,
-                border_bottom=border_bt,
-                border_top=border_bt,
+                border_right=border_right_left,
+                border_left=border_right_left,
+                border_bottom=border_top_bottom,
+                border_top=border_top_bottom,
             )
             name = document.insert_style(style=style, automatic=True)
             cell = Cell(value=rgb2hex(cell_value), style=name)
@@ -82,10 +97,39 @@ def generate_chart():
     return document
 
 
-def main():
-    hello_messages()
+def main() -> None:
     document = generate_chart()
+    test_unit(document)
     save_new(document, TARGET)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    table = document.body.get_table(name="chart")
+    assert isinstance(table, Table)
+
+    cell = table.get_cell("A1")
+    assert cell.value == "#000000"
+    style = document.get_style("table-cell", cell.style)
+    assert style.get_properties()["fo:background-color"] == cell.value
+
+    cell = table.get_cell("H1")
+    assert cell.value == "#E000E0"
+    style = document.get_style("table-cell", cell.style)
+    assert style.get_properties()["fo:background-color"] == cell.value
+
+    cell = table.get_cell("A32")
+    assert cell.value == "#00F8F8"
+    style = document.get_style("table-cell", cell.style)
+    assert style.get_properties()["fo:background-color"] == cell.value
+
+    cell = table.get_cell("H32")
+    assert cell.value == "#E0F8D8"
+    style = document.get_style("table-cell", cell.style)
+    assert style.get_properties()["fo:background-color"] == cell.value
 
 
 if __name__ == "__main__":
