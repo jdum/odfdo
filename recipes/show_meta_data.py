aff642aef728b0137057d7bab72e0a29ac27cb60
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-06 11:32:46 +0200

    refactor recipe show_meta_data

--- recipes/show_meta_data.py
+++ recipes/show_meta_data.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 """Print the metadata informations of a ODF file.
+
+Metadata are accessible through the meta part: meta = document.get_part("meta.xml")
+or the shortcut: document.meta.
+
+You then get access to various getters and setters. The getters return
+Python types and the respective setters take the same Python type as
+a parameter.
 """
+
+import os
 import sys
 from pathlib import Path
 
@@ -11,7 +20,8 @@ DATA = Path(__file__).parent / "data"
 SOURCE = "collection2.odt"
 
 
-def read_source_document():
+def read_source_document() -> Document:
+    """Return the source Document."""
     try:
         source = sys.argv[1]
     except IndexError:
@@ -19,48 +29,30 @@ def read_source_document():
     return Document(source)
 
 
-def main():
-    document = read_source_document()
+def print_meta(document: Document) -> None:
+    """Print the medadata of the document.
 
-    # Metadata are accessible through the meta part:
-    # meta = document.get_part("meta.xml")
-    # or the shortcut:
+    Output of the get_xxx methods for metadata.
+    Notice that odfdo doesn't increment editing cycles nor statistics
+    when saving the document.
+    For the metadata using dates or durations, odfdo provides datatypes that
+    decode from and serialize back to strings.
+    Strings are always decoded as utf-8, numeric values are decoded
+    as Decimal."""
+
     meta = document.meta
 
-    # You then get access to various getters and setters. The getters return
-    # Python types and the respective setters take the same Python type as
-    # a parameter.
-    #
-    # Here are the output of the get_xxx methods for metadata.
-    # (Notice that odfdo doesn't increment editing cycles nor statistics
-    # when saving the document.
-    # For the metadata using dates or durations, lpOD provides datatypes that
-    # decode from and serialize back to strings.
-    # Strings are always decoded as unicode, numeric values are always decoded
-    # as Decimal (as they offer the best precision).
-
-    print(f"Meta data of {document.container.path}")
-    # print("Title                :", meta.get_title())
+    print(f"Meta data of {document.path}")
     print("Title                :", meta.title)
-    # print("creator              :", meta.get_creator())
     print("creator              :", meta.creator)
-    # print("creation date        :", meta.get_creation_date())
     print("creation date        :", meta.creation_date)
-    # print("modification date    :", meta.get_modification_date())
     print("modification date    :", meta.date)
-    # print("initial creator      :", meta.get_initial_creator())
     print("initial creator      :", meta.initial_creator)
-    # print("subject              :", meta.get_subject())
     print("subject              :", meta.subject)
-    # print("description          :", meta.get_description())
     print("description          :", meta.description)
-    # print("editing cycles       :", meta.get_editing_cycles())
     print("editing cycles       :", meta.editing_cycles)
-    # print("editing duration     :", meta.get_editing_duration())
     print("editing duration     :", meta.editing_duration)
-    # print("generator            :", meta.get_generator())
     print("generator            :", meta.generator)
-    # print("language             :", meta.get_language())
     print("language             :", meta.language)
     print("keywords             :", meta.keyword)
     print("statistics    ")
@@ -76,6 +68,22 @@ def main():
     # A quick way to have all of those informations:
     print("-" * 70)
     print(document.get_formated_meta())
+
+
+def main() -> None:
+    document = read_source_document()
+    print_meta(document)
+    test_unit(document)
+
+
+def test_unit(document: Document) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    meta = document.meta
+    assert meta.keyword.strip() == "Douglas Adams"
+    assert meta.statistic["meta:page-count"] == 20
 
 
 if __name__ == "__main__":
