b449df2ad40d43f1ced73b3858c328f2c0ac4cb1
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-06 10:09:14 +0200

    refactor recipe save_document_as_bytesio

--- recipes/save_document_as_bytesio.py
+++ recipes/save_document_as_bytesio.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-"""Save a document as BytesIO.
-"""
+"""Save a document as BytesIO."""
+
 import io
+import os
 from pathlib import Path
 
 from odfdo import Document, Paragraph
@@ -11,7 +12,8 @@ OUTPUT_DIR = Path(__file__).parent / "recipes_output" / "bytes"
 TARGET = "document.odt"
 
 
-def make_document():
+def make_document() -> Document:
+    """Return a Hello World document."""
     document = Document("text")
     body = document.body
     paragraph = Paragraph("Hello World")
@@ -19,16 +21,38 @@ def make_document():
     return document
 
 
-def main():
-    document = make_document()
+def document_to_bytesio(document: Document) -> bytes:
     with io.BytesIO() as bytes_content:
         document.save(bytes_content)
         # Now use the BytesIO in some way:
         # In a netwotk context, typically:
         #    response.write(bytes_content.getvalue())
-        OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-        with open(OUTPUT_DIR / TARGET, "wb") as file:
-            file.write(bytes_content.getvalue())
+        return bytes_content.getvalue()
+
+
+def write_content(content: bytes) -> None:
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    path = OUTPUT_DIR / TARGET
+    path.write_bytes(content)
+
+
+def main() -> None:
+    document = make_document()
+    bytes_content = document_to_bytesio(document)
+    test_unit(bytes_content)
+    write_content(bytes_content)
+
+
+def test_unit(content: bytes) -> None:
+    # only for test suite:
+    if "ODFDO_TESTING" not in os.environ:
+        return
+
+    with io.BytesIO() as bytes_content:
+        bytes_content.write(content)
+        bytes_content.seek(0)
+        document = Document(bytes_content)
+        assert document.body.search("Hello World") is not None
 
 
 if __name__ == "__main__":
