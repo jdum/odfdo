401962dcf17128edacc26090edf9d0d6dab39725
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-13 10:09:33 +0200

    add more Unit conversions to pixel, add test for Unit

--- src/odfdo/unit.py
+++ src/odfdo/unit.py
@@ -18,7 +18,20 @@
 # The odfdo project is a derivative work of the lpod-python project:
 # https://github.com/lpod/lpod-python
 # Authors: Hervé Cauwelier <herve@itaapy.com>
-"""Unit class for conversion between ODF units and Python types."""
+"""Unit class for conversion between ODF units and Python types.
+
+Currently only distance -> pixel.
+
+mm: millimeter
+cm: centimeter
+m: meter
+km: kilometer
+pt: point
+pc: pica
+in: inch
+inch: inch
+ft: foot
+mi: mile"""
 
 from __future__ import annotations
 
@@ -73,9 +86,23 @@ class Unit:
     def convert(self, unit: str, dpi: int | Decimal | float = 72) -> Unit:
         """Convert from inch or cm to pixel."""
         if unit == "px":
-            if self.unit == "in":
+            if self.unit == "in" or self.unit == "inch":
                 return Unit(int(self.value * int(dpi)), "px")
             elif self.unit == "cm":
                 return Unit(int(self.value / Decimal("2.54") * int(dpi)), "px")
-            raise NotImplementedError(str(self.unit))
-        raise NotImplementedError(str(unit))
+            elif self.unit == "mm":
+                return Unit(int(self.value / Decimal("25.4") * int(dpi)), "px")
+            elif self.unit == "m":
+                return Unit(int(self.value / Decimal("0.0254") * int(dpi)), "px")
+            elif self.unit == "km":
+                return Unit(int(self.value / Decimal("0.0000254") * int(dpi)), "px")
+            elif self.unit == "pt":
+                return Unit(int(self.value / Decimal("72") * int(dpi)), "px")
+            elif self.unit == "pc":
+                return Unit(int(self.value / Decimal("6") * int(dpi)), "px")
+            elif self.unit == "ft":
+                return Unit(int(self.value * Decimal("12") * int(dpi)), "px")
+            elif self.unit == "mi":
+                return Unit(int(self.value * Decimal("63360") * int(dpi)), "px")
+            raise NotImplementedError(f"unit {str(self.unit)!r}")
+        raise NotImplementedError(f"unit {str(unit)!r}")
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -20,6 +20,8 @@
 # Authors: Hervé Cauwelier <herve@itaapy.com>
 from decimal import Decimal
 
+import pytest
+
 from odfdo.unit import Unit
 
 
@@ -97,3 +99,105 @@ def test_ge():
 def test_convert():
     unit = Unit("10cm")
     assert unit.convert("px") == Unit("283px")
+
+
+def test_no_nondigit():
+    unit = Unit("2.54")
+    assert unit.unit == "cm"
+
+
+def test_no_nondigit_2():
+    unit = Unit("12", "mm")
+    assert unit.unit == "mm"
+
+
+def test_wrong_cmp_unit_1():
+    unit = Unit("12", "mm")
+    unit2 = ["wrong"]
+    assert unit != unit2
+
+
+def test_wrong_cmp_unit_2():
+    unit = Unit("12", "mm")
+    unit2 = ["wrong"]
+    with pytest.raises(TypeError):
+        unit._check_other(unit2)
+
+
+def test_wrong_cmp_unit_3():
+    unit = Unit("12", "mm")
+    unit2 = Unit("12", "km")
+    with pytest.raises(NotImplementedError):
+        unit._check_other(unit2)
+
+
+def test_wrong_unit_convert():
+    unit = Unit("12", "cm")
+    with pytest.raises(NotImplementedError):
+        unit.convert("km")
+
+
+def test_wrong_unit_convert_2():
+    unit = Unit("12", "micron")
+    with pytest.raises(NotImplementedError):
+        unit.convert("px")
+
+
+def test_convert_1():
+    unit = Unit("10", "in")
+    converted = unit.convert("px")
+    assert converted.value == 720
+
+
+def test_convert_2():
+    unit = Unit("10", "inch")
+    converted = unit.convert("px")
+    assert converted.value == 720
+
+
+def test_convert_3():
+    unit = Unit("254", "cm")
+    converted = unit.convert("px")
+    assert converted.value == 7200
+
+
+def test_convert_4():
+    unit = Unit("254", "mm")
+    converted = unit.convert("px")
+    assert converted.value == 720
+
+
+def test_convert_5():
+    unit = Unit("254", "m")
+    converted = unit.convert("px")
+    assert converted.value == 720000
+
+
+def test_convert_6():
+    unit = Unit("254", "km")
+    converted = unit.convert("px")
+    assert converted.value == 720000000
+
+
+def test_convert_7():
+    unit = Unit("72", "pt")
+    converted = unit.convert("px")
+    assert converted.value == 72
+
+
+def test_convert_8():
+    unit = Unit("1", "pc")
+    converted = unit.convert("px")
+    assert converted.value == 12
+
+
+def test_convert_9():
+    unit = Unit("1", "ft")
+    converted = unit.convert("px")
+    assert converted.value == 864
+
+
+def test_convert_10():
+    unit = Unit("1", "mi")
+    converted = unit.convert("px")
+    assert converted.value == 4561920
