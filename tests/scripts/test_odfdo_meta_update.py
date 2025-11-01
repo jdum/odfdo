7825fed9833469d8e244c6d59ae6b0e8f0621ccf
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-11-01 11:02:17 +0100

    refactor test test_odfdo_meta_update.py

--- tests/scripts/test_odfdo_meta_update.py
+++ tests/scripts/test_odfdo_meta_update.py
@@ -7,8 +7,12 @@ import subprocess
 import sys
 from pathlib import Path
 
+import pytest
+
 from odfdo import Document
 from odfdo.scripts import meta_update
+from odfdo.scripts.meta_update import main as main_script
+from odfdo.scripts.meta_update import main_meta_update, parse_cli_args
 
 SCRIPT = Path(meta_update.__file__)
 SOURCE = "meta.odt"
@@ -45,26 +49,59 @@ def test_meta_update_no_param():
     assert b"Error:" in out
 
 
-def test_meta_update_no_file():
-    params = ["-i", "none_file1", "-o", "none_file2"]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 1
-    assert b"usage:" in out
-    assert b"Error" in out
+# direct access to internal function
 
 
-def test_meta_update_no_json_file():
-    params = ["-j"]
-    _out, _err, exitcode = run_params(params)
-    assert exitcode >= 1
+def test_meta_update_2_no_param_on_main_function(monkeypatch, capsys):
+    with pytest.raises(SystemExit) as result:
+        monkeypatch.setattr(sys, "argv", [])
+        main_script()
+        assert result.type is SystemExit
+        assert result.value.code >= 1
+    captured = capsys.readouterr()
+
+    assert "usage" in captured.out
 
 
-def test_meta_update_strip(samples):
+def test_meta_update_2_no_param(capsys):
+    params = parse_cli_args([])
+
+    with pytest.raises(SystemExit) as result:
+        main_meta_update(params)
+        assert result.type is SystemExit
+        assert result.value.code == 1
+    captured = capsys.readouterr()
+
+    assert "usage" in captured.out
+
+
+def test_meta_update_2_no_file(capsys):
+    params = parse_cli_args(["-i", "none_file1", "-o", "none_file2"])
+
+    with pytest.raises(SystemExit) as result:
+        main_meta_update(params)
+        assert result.type is SystemExit
+        assert result.value.code == 1
+    captured = capsys.readouterr()
+
+    assert "usage" in captured.out
+    assert "FileNotFoundError" in captured.out
+
+
+def test_meta_update_2_no_json_file(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["-j"])
+        assert result.value.code >= 1
+
+
+def test_meta_update_2_strip(capsysbinary, samples):
     source = str(samples(SOURCE))
-    params = ["-s", "-i", source]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    content = io.BytesIO(out)
+    params = parse_cli_args(["-s", "-i", source])
+
+    main_meta_update(params)
+    captured = capsysbinary.readouterr()
+
+    content = io.BytesIO(captured.out)
     document = Document(content)
     content.close()
     metadata = document.meta.as_dict(full=True)
@@ -78,15 +115,17 @@ def test_meta_update_strip(samples):
     assert metadata["meta:user-defined"] == []
 
 
-def test_meta_update_json_changed(tmp_path, samples):
+def test_meta_update_2_json_changed(capsysbinary, tmp_path, samples):
     source = str(samples(SOURCE))
     json_string = json.dumps(JSON_CONTENT)
     jpath = tmp_path / "json_data.json"
     jpath.write_text(json_string, encoding="utf8")
-    params = ["-i", source, "-j", str(jpath)]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    content = io.BytesIO(out)
+    params = parse_cli_args(["-i", source, "-j", str(jpath)])
+
+    main_meta_update(params)
+    captured = capsysbinary.readouterr()
+
+    content = io.BytesIO(captured.out)
     document = Document(content)
     content.close()
     metadata = document.meta.as_dict(full=True)
@@ -94,47 +133,82 @@ def test_meta_update_json_changed(tmp_path, samples):
     assert metadata["meta:generator"] == "my generator"
 
 
-def test_meta_update_json_removed(tmp_path, samples):
+def test_meta_update_2_json_removed(capsysbinary, tmp_path, samples):
     source = str(samples(SOURCE))
     json_string = json.dumps(JSON_CONTENT)
     jpath = tmp_path / "json_data.json"
     jpath.write_text(json_string, encoding="utf8")
-    params = ["-i", source, "-j", str(jpath)]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    content = io.BytesIO(out)
+    params = parse_cli_args(["-i", source, "-j", str(jpath)])
+
+    main_meta_update(params)
+    captured = capsysbinary.readouterr()
+
+    content = io.BytesIO(captured.out)
     document = Document(content)
     content.close()
     metadata = document.meta.as_dict(full=True)
     assert metadata["dc:subject"] is None
 
 
-def test_meta_update_json_kept(tmp_path, samples):
+def test_meta_update_2_json_kept(capsysbinary, tmp_path, samples):
     source = str(samples(SOURCE))
     json_string = json.dumps(JSON_CONTENT)
     jpath = tmp_path / "json_data.json"
     jpath.write_text(json_string, encoding="utf8")
-    params = ["-i", source, "-j", str(jpath)]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    content = io.BytesIO(out)
+    params = parse_cli_args(["-i", source, "-j", str(jpath)])
+
+    main_meta_update(params)
+    captured = capsysbinary.readouterr()
+
+    content = io.BytesIO(captured.out)
     document = Document(content)
     content.close()
     metadata = document.meta.as_dict(full=True)
     assert metadata["dc:title"] == "Intitulé"
 
 
-def test_meta_update_json_user(tmp_path, samples):
+def test_meta_update_2_json_user(capsysbinary, tmp_path, samples):
     source = str(samples(SOURCE))
     json_string = json.dumps(JSON_CONTENT)
     jpath = tmp_path / "json_data.json"
     jpath.write_text(json_string, encoding="utf8")
-    params = ["-i", source, "-j", str(jpath)]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    content = io.BytesIO(out)
+    params = parse_cli_args(["-i", source, "-j", str(jpath)])
+
+    main_meta_update(params)
+    captured = capsysbinary.readouterr()
+
+    content = io.BytesIO(captured.out)
     document = Document(content)
     content.close()
     metadata_user = document.meta.as_dict(full=True)["meta:user-defined"]
     assert len(metadata_user) == 5
     assert len([item for item in metadata_user if item["meta:name"] == "size"]) == 1
+
+
+def test_meta_update_2_json_user_out_put_path(tmp_path, samples):
+    source = str(samples(SOURCE))
+    json_string = json.dumps(JSON_CONTENT)
+    jpath = tmp_path / "json_data.json"
+    jpath.write_text(json_string, encoding="utf8")
+    dest_path = tmp_path / "output.odt"
+    params = parse_cli_args(["-i", source, "-j", str(jpath), "-o", str(dest_path)])
+
+    main_meta_update(params)
+
+    document = Document(dest_path)
+    metadata_user = document.meta.as_dict(full=True)["meta:user-defined"]
+    assert len(metadata_user) == 5
+    assert len([item for item in metadata_user if item["meta:name"] == "size"]) == 1
+
+
+def test_meta_update_2_no_json_or_strip(capsys, samples):
+    source = str(samples(SOURCE))
+    params = parse_cli_args(["-i", source])
+
+    with pytest.raises(SystemExit) as result:
+        main_meta_update(params)
+        assert result.type is SystemExit
+        assert result.value.code == 1
+    captured = capsys.readouterr()
+
+    assert "required" in captured.out
