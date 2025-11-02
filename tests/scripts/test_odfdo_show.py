cfc954c24eb11e25c67c4e712a4acfc62fe7992d
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-11-02 12:44:13 +0100

    refactor test_odfdo_show.py

--- tests/scripts/test_odfdo_show.py
+++ tests/scripts/test_odfdo_show.py
@@ -1,11 +1,16 @@
 # Copyright 2018-2025 Jérôme Dumonteil
 # Authors (odfdo project): jerome.dumonteil@gmail.com
+from __future__ import annotations
 
 import subprocess
 import sys
 from pathlib import Path
 
+import pytest
+
 from odfdo.scripts import show
+from odfdo.scripts.show import main as main_script
+from odfdo.scripts.show import main_show, parse_cli_args
 
 SCRIPT = Path(show.__file__)
 
@@ -22,7 +27,7 @@ def run_params(params: list):
     return out, err, proc.returncode
 
 
-def test_no_param():
+def test_show_no_param():
     params = []
     _out, err, exitcode = run_params(params)
     assert exitcode == 2
@@ -30,113 +35,175 @@ def test_no_param():
     assert "usage" in err
 
 
-def test_help():
-    params = ["--help"]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert "Dump text from an OpenDocument" in out
+# direct access to internal function
 
 
-def test_no_file():
-    params = ["none_file"]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 1
-    assert "FileNotFoundError" in out
+def test_show_2_no_param_on_main_function(monkeypatch):
+    with pytest.raises(SystemExit) as result:
+        monkeypatch.setattr(sys, "argv", [])
+        main_script()
+        assert result.value.code >= 1
 
 
-def test_base(samples):
+def test_show_2_no_param():
+    with pytest.raises(SystemExit) as result:
+        params = parse_cli_args([])
+        main_show(params)
+        assert result.value.code >= 1
+
+
+def test_show_2_version(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["--version"])
+        assert result.value.code == 0
+    captured = capsys.readouterr()
+
+    assert "odfdo-show v3" in captured.out
+
+
+def test_show_2_help(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["--help"])
+        assert result.value.code == 0
+    captured = capsys.readouterr()
+
+    assert "Dump text from an OpenDocument" in captured.out
+
+
+def test_show_2_no_file():
+    params = parse_cli_args(["none_file"])
+
+    with pytest.raises(SystemExit) as result:
+        main_show(params)
+        assert result.value.code >= 1
+
+
+def test_show_2_base_on_main_function(capsys, monkeypatch, samples):
     source = str(samples("base_text.odt"))
-    params = [source]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert "This is the second paragraph." in out
+    monkeypatch.setattr(sys, "argv", ["odfdo-replace", source])
+
+    main_script()
+    captured = capsys.readouterr()
+
+    assert "This is the second paragraph." in captured.out
 
 
-def test_no_text(samples):
+def test_show_2_base(capsys, samples):
     source = str(samples("base_text.odt"))
-    params = ["-n", source]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert not out.strip()
+    params = parse_cli_args([source])
+
+    main_show(params)
+    captured = capsys.readouterr()
+
+    assert "This is the second paragraph." in captured.out
 
 
-def test_meta(samples):
+def test_show_2_no_text(capsys, samples):
     source = str(samples("base_text.odt"))
-    params = ["-nm", source]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert "This is the second paragraph." not in out
-    assert "Keyword: These are the keywords" in out
-    assert "Generator: LibreOffice" in out
+    params = parse_cli_args(["-n", source])
+
+    main_show(params)
+    captured = capsys.readouterr()
+
+    assert not captured.out.strip()
 
 
-def test_style(samples):
+def test_show_2_meta(capsys, samples):
     source = str(samples("base_text.odt"))
-    params = ["-s", source]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert "This is the second paragraph." in out
-    assert "used:y family:paragraph" in out
+    params = parse_cli_args(["-nm", source])
+
+    main_show(params)
+    captured = capsys.readouterr()
+
+    assert "This is the second paragraph." not in captured.out
+    assert "Keyword: These are the keywords" in captured.out
+    assert "Generator: LibreOffice" in captured.out
 
 
-def test_style_no_text(samples):
+def test_show_2_style(capsys, samples):
     source = str(samples("base_text.odt"))
-    params = ["-ns", source]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert "This is the second paragraph." not in out
-    assert "used:y family:paragraph" in out
+    params = parse_cli_args(["-s", source])
+
+    main_show(params)
+    captured = capsys.readouterr()
+
+    assert "This is the second paragraph." in captured.out
+    assert "used:y family:paragraph" in captured.out
 
 
-def test_style_rst(samples):
+def test_show_2_style_no_text(capsys, samples):
     source = str(samples("base_text.odt"))
-    params = ["-r", source]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert "Level 2 Title\n=============" in out
+    params = parse_cli_args(["-ns", source])
+
+    main_show(params)
+    captured = capsys.readouterr()
+
+    assert "This is the second paragraph." not in captured.out
+    assert "used:y family:paragraph" in captured.out
 
 
-def test_style_ods(samples):
+def test_show_2_style_rst(capsys, samples):
+    source = str(samples("base_text.odt"))
+    params = parse_cli_args(["-r", source])
+
+    main_show(params)
+    captured = capsys.readouterr()
+
+    assert "Level 2 Title\n=============" in captured.out
+
+
+def test_show_2_style_ods(capsys, samples):
     source = str(samples("styled_table.ods"))
-    params = [source]
-    out, _err, exitcode = run_params(params)
-    print(out)
-    assert exitcode == 0
-    assert "1,2,3,4" in out
+    params = parse_cli_args([source])
+
+    main_show(params)
+    captured = capsys.readouterr()
+
+    assert "1,2,3,4" in captured.out
 
 
-def test_not_style_ods(samples):
+def test_show_2_not_style_ods(capsys, samples):
     source = str(samples("styled_table.ods"))
-    params = ["-n", source]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert "1,2,3,4" not in out
+    params = parse_cli_args(["-n", source])
+
+    main_show(params)
+    captured = capsys.readouterr()
+
+    assert "1,2,3,4" not in captured.out
 
 
-def test_unsupported(samples):
+def test_show_2_unsupported(capsys, samples):
     source = str(samples("base_shapes.odg"))
-    params = [source]
-    out, err, exitcode = run_params(params)
-    assert exitcode == 1
-    print(out)
-    assert "format 'graphics' is not supported" in err
+    params = parse_cli_args([source])
+
+    with pytest.raises(SystemExit) as result:
+        main_show(params)
+        assert result.value.code >= 1
+    captured = capsys.readouterr()
+
+    assert "format 'graphics' is not supported" in captured.err
 
 
-def test_folder_unsupported(tmp_path, samples):
+def test_show_2_folder_unsupported(tmp_path, capsys, samples):
     source = str(samples("base_shapes.odg"))
     dest = tmp_path / "test_show"
-    params = ["-o", f"{dest}", source]
-    _out, err, exitcode = run_params(params)
-    assert exitcode == 1
-    assert "format 'graphics' is not supported" in err
+    params = parse_cli_args(["-o", str(dest), source])
+
+    with pytest.raises(SystemExit) as result:
+        main_show(params)
+        assert result.value.code >= 1
+    captured = capsys.readouterr()
+
+    assert "format 'graphics' is not supported" in captured.err
 
 
-def test_folder_text(tmp_path, samples):
+def test_show_2_folder_text(tmp_path, samples):
     source = str(samples("base_text.odt"))
     dest = tmp_path / "test_show"
-    params = ["-o", f"{dest}", source]
-    _out, _err, exitcode = run_params(params)
-    assert exitcode == 0
+    params = parse_cli_args(["-o", str(dest), source])
+
+    main_show(params)
+
     assert dest.is_dir()
     assert (dest / "meta.txt").is_file()
     assert (dest / "styles.txt").is_file()
@@ -144,12 +211,13 @@ def test_folder_text(tmp_path, samples):
     assert not (dest / "Pictures").exists()
 
 
-def test_folder_image(tmp_path, samples):
+def test_show_2_folder_image(tmp_path, samples):
     source = str(samples("background.odp"))
     dest = tmp_path / "test_show"
-    params = ["-o", f"{dest}", source]
-    _out, _err, exitcode = run_params(params)
-    assert exitcode == 0
+    params = parse_cli_args(["-o", str(dest), source])
+
+    main_show(params)
+
     assert dest.is_dir()
     assert (dest / "meta.txt").is_file()
     assert (dest / "styles.txt").is_file()
@@ -160,12 +228,13 @@ def test_folder_image(tmp_path, samples):
     ).is_file()
 
 
-def test_folder_ods(tmp_path, samples):
+def test_show_2_folder_ods(tmp_path, samples):
     source = str(samples("styled_table.ods"))
     dest = tmp_path / "test_show_ods"
-    params = ["-o", f"{dest}", source]
-    _out, _err, exitcode = run_params(params)
-    assert exitcode == 0
+    params = parse_cli_args(["-o", str(dest), source])
+
+    main_show(params)
+
     assert dest.is_dir()
     assert (dest / "meta.txt").is_file()
     assert (dest / "styles.txt").is_file()
