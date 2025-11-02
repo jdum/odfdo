04fb33e25a8ff5235fe26dff394b21bbec1a5b46
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-11-02 11:10:57 +0100

    refactor script from_csv.py

--- src/odfdo/scripts/from_csv.py
+++ src/odfdo/scripts/from_csv.py
@@ -76,14 +76,15 @@ def configure_parser() -> ArgumentParser:
     return parser
 
 
-def error(message: str) -> str:
-    return f"\n{PROG}: {message}"
+def parse_cli_args(cli_args: list[str] | None = None) -> Namespace:
+    parser = configure_parser()
+    return parser.parse_args(cli_args)
 
 
 def read_document(input_file: str | None) -> str:
     if input_file:
         csv_content = Path(input_file).read_text()
-    else:  # prgma nocover
+    else:  # pragma: no cover
         detect_stdin_timeout()
         content = io.BytesIO(sys.stdin.buffer.read())
         csv_content = content.getvalue().decode()
@@ -104,16 +105,19 @@ def from_csv(args: Namespace) -> None:
 
 
 def main() -> int:
-    parser = configure_parser()
-    args = parser.parse_args()
+    args: Namespace = parse_cli_args()
+    return main_from_csv(args)
+
+
+def main_from_csv(args: Namespace) -> int:
     try:
         from_csv(args)
     except Exception:
-        parser.print_help()
+        configure_parser().print_help()
         print()
         raise
     return 0
 
 
-if __name__ == "__main__":  # pragma: no cover
+if __name__ == "__main__":
     raise SystemExit(main())
