232692bcf9954a5e19c82742c0f7558e46b08c83
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-11-08 11:50:18 +0100

    refactor test_odfdo_userfield.py

--- tests/scripts/test_odfdo_userfield.py
+++ tests/scripts/test_odfdo_userfield.py
@@ -1,13 +1,18 @@
 # Copyright 2018-2025 Jérôme Dumonteil
 # Authors (odfdo project): jerome.dumonteil@gmail.com
+from __future__ import annotations
 
 import io
 import subprocess
 import sys
 from pathlib import Path
 
-from odfdo import Document
+import pytest
+
+from odfdo.document import Document
 from odfdo.scripts import userfield
+from odfdo.scripts.userfield import main as main_script
+from odfdo.scripts.userfield import main_userfields, parse_cli_args
 
 SCRIPT = Path(userfield.__file__)
 SOURCE = "user_fields.odt"
@@ -24,7 +29,7 @@ def run_params(params: list):
     return out, err, proc.returncode
 
 
-def test_no_param():
+def test_userfields_no_param():
     params = []
     out, _err, exitcode = run_params(params)
     assert exitcode == 1
@@ -32,104 +37,202 @@ def test_no_param():
     assert b"Error: ValueError, missing arguments" in out
 
 
-def test_no_file():
-    params = ["-i", "none_file1", "-o", "none_file2", "-anr"]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 1
-    assert b"usage:" in out
-    assert b"FileNotFoundError" in out
+# direct access to internal function
 
 
-def test_field(samples):
+def test_userfields_2_no_param_on_main_function(monkeypatch):
+    with pytest.raises(SystemExit) as result:
+        monkeypatch.setattr(sys, "argv", [])
+        main_script()
+        assert result.value.code >= 1
+
+
+def test_userfields_2_no_param():
+    with pytest.raises(SystemExit) as result:
+        params = parse_cli_args([])
+        main_userfields(params)
+        assert result.value.code >= 1
+
+
+def test_userfields_2_version(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["--version"])
+        assert result.value.code == 0
+    captured = capsys.readouterr()
+
+    assert "odfdo-userfield v3" in captured.out
+
+
+def test_userfields_2_help(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["--help"])
+        assert result.value.code == 0
+    captured = capsys.readouterr()
+
+    assert "usage: odfdo-userfield" in captured.out
+
+
+def test_userfields_2_no_file():
+    params = parse_cli_args(["-i", "none_file1", "-o", "none_file2", "-anr"])
+
+    with pytest.raises(SystemExit) as result:
+        main_userfields(params)
+        assert result.value.code >= 1
+
+
+def test_userfields_2_field(capsys, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-f", "city"]
-    out, _err, _exitcode = run_params(params)
-    assert b"Paris" in out
+    params = parse_cli_args(["-i", source, "-f", "city"])
+
+    main_userfields(params)
+    captured = capsys.readouterr()
+
+    assert "Paris" in captured.out
 
 
-def test_field2(samples):
+def test_userfields_2_field2(capsys, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-f", "city", "-f", "phone"]
-    out, _err, _exitcode = run_params(params)
-    assert b"Paris" in out
-    assert b"77 77 77 77 77" in out
+    params = parse_cli_args(["-i", source, "-f", "city", "-f", "phone"])
+
+    main_userfields(params)
+    captured = capsys.readouterr()
+
+    assert "Paris" in captured.out
+    assert "77 77 77 77 77" in captured.out
 
 
-def test_field3(samples):
+def test_userfields_2_field3(capsys, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-f", "city", "-f", "phone", "-f", "counter"]
-    out, _err, _exitcode = run_params(params)
-    assert b"Paris" in out
-    assert b"77 77 77 77 77" in out
-    assert b"42" in out
+    params = parse_cli_args(
+        ["-i", source, "-f", "city", "-f", "phone", "-f", "counter"]
+    )
+
+    main_userfields(params)
+    captured = capsys.readouterr()
+
+    assert "Paris" in captured.out
+    assert "77 77 77 77 77" in captured.out
+    assert "42" in captured.out
 
 
-def test_all(samples):
+def test_userfields_2_all(capsys, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-a"]
-    out, _err, _exitcode = run_params(params)
-    assert b"Paris" in out
-    assert b"77 77 77 77 77" in out
-    assert b"42" in out
-    assert b"city" not in out
-    assert b"string" not in out
+    params = parse_cli_args(["-i", source, "-a"])
+
+    main_userfields(params)
+    captured = capsys.readouterr()
+
+    assert "Paris" in captured.out
+    assert "77 77 77 77 77" in captured.out
+    assert "42" in captured.out
+    assert "city" not in captured.out
+    assert "string" not in captured.out
 
 
-def test_all_name(samples):
+def test_userfields_2_all_name(capsys, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-an"]
-    out, _err, _exitcode = run_params(params)
-    assert b"Paris" in out
-    assert b"77 77 77 77 77" in out
-    assert b"42" in out
-    assert b"city" in out
-    assert b"string" not in out
+    params = parse_cli_args(["-i", source, "-an"])
+
+    main_userfields(params)
+    captured = capsys.readouterr()
+
+    assert "Paris" in captured.out
+    assert "77 77 77 77 77" in captured.out
+    assert "42" in captured.out
+    assert "city" in captured.out
+    assert "string" not in captured.out
 
 
-def test_all_name_type(samples):
+def test_userfields_2_all_name_type(capsys, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-ant"]
-    out, _err, _exitcode = run_params(params)
-    assert b"Paris" in out
-    assert b"77 77 77 77 77" in out
-    assert b"42" in out
-    assert b"city" in out
-    assert b"string" in out
+    params = parse_cli_args(["-i", source, "-ant"])
+
+    main_userfields(params)
+    captured = capsys.readouterr()
+
+    assert "Paris" in captured.out
+    assert "77 77 77 77 77" in captured.out
+    assert "42" in captured.out
+    assert "city" in captured.out
+    assert "string" in captured.out
 
 
-def test_all_name_type_repr(samples):
+def test_userfields_2_all_name_type_repr(capsys, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-antr"]
-    out, _err, _exitcode = run_params(params)
-    assert b"Paris" in out
-    assert b"77 77 77 77 77" in out
-    assert b"42" in out
-    assert b"city" in out
-    assert b"string" in out
+    params = parse_cli_args(["-i", source, "-antr"])
+
+    main_userfields(params)
+    captured = capsys.readouterr()
+
+    assert "Paris" in captured.out
+    assert "77 77 77 77 77" in captured.out
+    assert "42" in captured.out
+    assert "city" in captured.out
+    assert "string" in captured.out
 
 
-def test_set_err(samples):
+def test_userfields_2_set_err(capsys, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-s", "city"]
-    _out, err, _exitcode = run_params(params)
-    assert b"expected 2 arguments" in err
+
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["-i", source, "-s", "city"])
+        assert result.value.code >= 1
+    captured = capsys.readouterr()
+
+    assert "expected 2 arguments" in captured.err
 
 
-def test_set(samples):
+def test_userfields_2_set(capsysbinary, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-s", "city", "Lyon"]
-    out, _err, _exitcode = run_params(params)
-    content = io.BytesIO(out)
+    params = parse_cli_args(["-i", source, "-s", "city", "Lyon"])
+
+    main_userfields(params)
+    captured = capsysbinary.readouterr()
+
+    content = io.BytesIO(captured.out)
     document = Document(content)
     content.close()
     assert document.body.get_user_field_value("city") == "Lyon"
 
 
-def test_set2(samples):
+def test_userfields_2_set2(capsysbinary, samples):
     source = str(samples(SOURCE))
-    params = ["-i", source, "-s", "city", "Lyon", "-s", "counter", "99"]
-    out, _err, _exitcode = run_params(params)
-    content = io.BytesIO(out)
+    params = parse_cli_args(["-i", source, "-s", "city", "Lyon", "-s", "counter", "99"])
+
+    main_userfields(params)
+    captured = capsysbinary.readouterr()
+
+    content = io.BytesIO(captured.out)
+    document = Document(content)
+    content.close()
+    assert document.body.get_user_field_value("city") == "Lyon"
+    assert document.body.get_user_field_value("counter") == "99"
+
+
+def test_userfields_2_set3_unknown(capsysbinary, samples):
+    source = str(samples(SOURCE))
+    params = parse_cli_args(
+        [
+            "-i",
+            source,
+            "-s",
+            "city",
+            "Lyon",
+            "-s",
+            "counter",
+            "99",
+            "-s",
+            "unknown_field",
+            "nothing",
+        ]
+    )
+
+    main_userfields(params)
+    captured = capsysbinary.readouterr()
+
+    assert b"unknown user-field" in captured.err
+
+    content = io.BytesIO(captured.out)
     document = Document(content)
     content.close()
     assert document.body.get_user_field_value("city") == "Lyon"
