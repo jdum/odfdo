6413bc3ef3d390d7d3e0411cc763eb56ca48d6ad
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-11-08 11:08:52 +0100

    refactor test_odfdo_markdown.py

--- tests/scripts/test_odfdo_markdown.py
+++ tests/scripts/test_odfdo_markdown.py
@@ -1,11 +1,16 @@
 # Copyright 2018-2025 Jérôme Dumonteil
 # Authors (odfdo project): jerome.dumonteil@gmail.com
+from __future__ import annotations
 
 import subprocess
 import sys
 from pathlib import Path
 
+import pytest
+
 from odfdo.scripts import to_markdown
+from odfdo.scripts.to_markdown import main as main_script
+from odfdo.scripts.to_markdown import main_to_md, parse_cli_args
 
 SCRIPT = Path(to_markdown.__file__)
 
@@ -22,43 +27,70 @@ def run_params(params: list):
     return out, err, proc.returncode
 
 
-def test_no_param():
+def test_to_md_no_param():
     params = []
     out, _err, exitcode = run_params(params)
     assert exitcode == 1
     assert "usage:" in out
 
 
-def test_version():
-    params = ["--version"]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 0
-    assert "odfdo-markdown v3" in out
+# direct access to internal function
 
 
-def test_no_file():
-    params = ["none_file"]
-    out, _err, exitcode = run_params(params)
-    assert exitcode == 1
-    assert "Export text document in markdown format to stdout" in out
+def test_to_md_2_no_param_on_main_function(monkeypatch):
+    with pytest.raises(ValueError) as result:
+        monkeypatch.setattr(sys, "argv", [])
+        main_script()
+        assert result.value.code >= 1
 
 
-def test_md_osq_bad(samples):
+def test_to_md_2_no_param():
+    with pytest.raises(ValueError) as result:
+        params = parse_cli_args([])
+        main_to_md(params)
+        assert result.value.code >= 1
+
+
+def test_to_md_2_version(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["--version"])
+        assert result.value.code == 0
+    captured = capsys.readouterr()
+
+    assert "odfdo-markdown v3" in captured.out
+
+
+def test_to_md_2_help(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["--help"])
+        assert result.value.code == 0
+    captured = capsys.readouterr()
+
+    assert "document in markdown format" in captured.out
+
+
+def test_to_md_2_no_file():
+    params = parse_cli_args(["none_file"])
+    with pytest.raises(FileNotFoundError) as result:
+        main_to_md(params)
+        assert result.value.code >= 1
+
+
+def test_to_md_2_md_osq_bad(samples):
     source = samples("simple_table.ods")
-    params = [source]
-    out, err, exitcode = run_params(params)
-    assert exitcode == 1
-    assert "usage:" in out
-    assert "Type of document 'spreadsheet' not supported" in err
+    params = parse_cli_args([str(source)])
+
+    with pytest.raises(NotImplementedError) as result:
+        main_to_md(params)
+        assert result.value.code >= 1
 
 
-def test_md_1(samples):
+def test_to_md_2_example(capsys, samples):
     source = samples("example.odt")
-    params = [source]
-    print(source)
-    out, err, exitcode = run_params(params)
-    print(out)
-    print(err)
-    assert exitcode == 0
-    assert "odfdo Test Case Document" in out
-    assert "First paragraph" in out
+    params = parse_cli_args([str(source)])
+
+    main_to_md(params)
+    captured = capsys.readouterr()
+
+    assert "odfdo Test Case Document" in captured.out
+    assert "First paragraph" in captured.out
