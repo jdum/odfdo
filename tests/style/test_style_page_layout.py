70fbbba64204198484281efbb54912331e060b03
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-11-15 15:46:45 +0100

    add tests for page_layout.py

--- tests/style/test_style_page_layout.py
+++ tests/style/test_style_page_layout.py
@@ -1,5 +1,4 @@
 # Copyright 2018-2025 Jérôme Dumonteil
-# Copyright (c) 2009-2010 Ars Aperta, Itaapy, Pierlis, Talend.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -49,6 +48,36 @@ def test_style_page_layout_from_tag():
 def test_style_page_layout_repr():
     spl = Element.from_tag("<style:page-layout/>")
     assert repr(spl) == "<StylePageLayout family=page-layout name=None>"
+
+
+def test_style_page_layout_create_name():
+    spl = StylePageLayout(name="a_name")
+    assert spl.serialize() == '<style:page-layout style:name="a_name"/>'
+
+
+def test_style_page_layout_page_usage_1():
+    spl = StylePageLayout(page_usage="bad")
+    assert spl.serialize() == "<style:page-layout/>"
+
+
+def test_style_page_layout_page_usage_2():
+    spl = StylePageLayout(page_usage="all")
+    assert spl.serialize() == "<style:page-layout/>"
+
+
+def test_style_page_layout_page_usage_3():
+    spl = StylePageLayout(page_usage="left")
+    assert spl.serialize() == '<style:page-layout style:page-usage="left"/>'
+
+
+def test_style_page_layout_page_usage_4():
+    spl = StylePageLayout(page_usage="right")
+    assert spl.serialize() == '<style:page-layout style:page-usage="right"/>'
+
+
+def test_style_page_layout_page_usage_5():
+    spl = StylePageLayout(page_usage="mirrored")
+    assert spl.serialize() == '<style:page-layout style:page-usage="mirrored"/>'
 
 
 def test_get_style_page_layout(styles):
@@ -119,11 +148,116 @@ def test_get_style_page_layout_get_properties(styles):
     assert props == expected
 
 
+def test_get_style_page_layout_set_properties(styles):
+    auto = styles.office_automatic_styles
+    spl = auto.page_layouts[0]
+    new_props = {
+        "fo:page-width": "10.999cm",
+        "fo:page-height": "19.699cm",
+        "style:num-format": "1",
+        "style:print-orientation": "portrait",
+        "fo:margin-top": "4cm",
+        "fo:margin-bottom": "4cm",
+        "fo:margin-left": "4cm",
+        "fo:margin-right": "4cm",
+        "style:writing-mode": "lr-tb",
+        "style:layout-grid-color": "#cccccc",
+        "style:layout-grid-lines": "10",
+        "style:layout-grid-base-height": "0.706cm",
+        "style:layout-grid-ruby-height": "0.353cm",
+        "style:layout-grid-mode": "none",
+        "style:layout-grid-ruby-below": "false",
+        "style:layout-grid-print": "true",
+        "style:layout-grid-display": "false",
+        "style:footnote-max-height": "0cm",
+        "style:footnote-sep": {
+            "style:width": "0.018cm",
+            "style:distance-before-sep": "0.101cm",
+            "style:distance-after-sep": "0.101cm",
+            "style:line-style": "none",
+            "style:adjustment": "left",
+            "style:rel-width": "25%",
+            "style:color": "#000000",
+        },
+    }
+    spl.set_properties(new_props)
+    props = spl.get_properties()
+    expected = new_props
+    assert props == expected
+
+
+def test_get_style_page_layout_del_properties(styles):
+    auto = styles.office_automatic_styles
+    spl = auto.page_layouts[0]
+    expected = {
+        "fo:page-width": "20.999cm",
+        "fo:page-height": "29.699cm",
+        "style:num-format": "1",
+        "style:print-orientation": "portrait",
+        "fo:margin-left": "2cm",
+        "fo:margin-right": "2cm",
+        "style:writing-mode": "lr-tb",
+        "style:layout-grid-color": "#c0c0c0",
+        "style:layout-grid-lines": "20",
+        "style:layout-grid-base-height": "0.706cm",
+        "style:layout-grid-ruby-height": "0.353cm",
+        "style:layout-grid-mode": "none",
+        "style:layout-grid-ruby-below": "false",
+        "style:layout-grid-print": "false",
+        "style:layout-grid-display": "false",
+        "style:footnote-max-height": "0cm",
+        "style:footnote-sep": {
+            "style:width": "0.018cm",
+            "style:distance-before-sep": "0.101cm",
+            "style:distance-after-sep": "0.101cm",
+            "style:line-style": "none",
+            "style:adjustment": "left",
+            "style:rel-width": "25%",
+            "style:color": "#000000",
+        },
+    }
+    spl.del_properties(["fo:margin-top", "fo:margin-bottom"])
+    props = spl.get_properties()
+    assert props == expected
+
+
+def test_get_style_page_layout_set_background():
+    spl = StylePageLayout(name="a_name")
+    spl.set_background(color="#666666", position="center")
+    assert spl.serialize() == (
+        '<style:page-layout style:name="a_name">'
+        '<style:page-layout-properties fo:background-color="#666666"/>'
+        "</style:page-layout>"
+    )
+
+
 def test_get_style_page_layout_get_header_style(styles):
     auto = styles.office_automatic_styles
     spl = auto.page_layouts[0]
     style = spl.get_header_style()
     assert style.tag == "style:header-style"
+
+
+def test_get_style_page_layout_set_header_style(styles):
+    auto = styles.office_automatic_styles
+    spl = auto.page_layouts[0]
+    new_style = Element.from_tag("style:header-style")
+    spl.set_header_style(new_style)
+    style = spl.get_header_style()
+    assert style.tag == "style:header-style"
+    assert style.children == []
+
+
+def test_get_style_page_layout_set_header_style_2(styles):
+    auto = styles.office_automatic_styles
+    spl = auto.page_layouts[0]
+    current_style = spl.get_header_style()
+    current_style.delete()
+    new_style = Element.from_tag("style:header-style")
+    spl.set_header_style(new_style)
+    style = spl.get_header_style()
+    assert style.tag == "style:header-style"
+    assert style.children == []
 
 
 def test_get_style_page_layout_get_footer_style(styles):
@@ -133,9 +267,23 @@ def test_get_style_page_layout_get_footer_style(styles):
     assert style.tag == "style:footer-style"
 
 
-# def test_set_style_page_layout(styles):
-#     new_oms = OfficeMasterStyles()
-#     styles.office_master_styles = new_oms
-#     oms = styles.office_master_styles
-#     assert isinstance(oms, OfficeMasterStyles)
-#     assert len(oms.children) == 0
+def test_get_style_page_layout_set_footer_style(styles):
+    auto = styles.office_automatic_styles
+    spl = auto.page_layouts[0]
+    new_style = Element.from_tag("style:footer-style")
+    spl.set_footer_style(new_style)
+    style = spl.get_footer_style()
+    assert style.tag == "style:footer-style"
+    assert style.children == []
+
+
+def test_get_style_page_layout_set_footer_style_2(styles):
+    auto = styles.office_automatic_styles
+    spl = auto.page_layouts[0]
+    current_style = spl.get_footer_style()
+    current_style.delete()
+    new_style = Element.from_tag("style:footer-style")
+    spl.set_footer_style(new_style)
+    style = spl.get_footer_style()
+    assert style.tag == "style:footer-style"
+    assert style.children == []
