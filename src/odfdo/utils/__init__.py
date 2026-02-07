7a97406dab8233a1eceb0ecf133dc9dcc58ea86d
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2026-02-07 09:25:09 +0100

    add docstring for module utils

--- src/odfdo/utils/__init__.py
+++ src/odfdo/utils/__init__.py
@@ -1,3 +1,24 @@
+# Copyright 2018-2026 Jérôme Dumonteil
+# Copyright (c) 2009-2010 Ars Aperta, Itaapy, Pierlis, Talend.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Authors (odfdo project): jerome.dumonteil@gmail.com
+# The odfdo project is a derivative work of the lpod-python project:
+# https://github.com/lpod/lpod-python
+"""Collection of utilities."""
+
 from .blob import Blob
 from .color import hex2rgb, hexa_color, rgb2hex
 from .coordinates import (
