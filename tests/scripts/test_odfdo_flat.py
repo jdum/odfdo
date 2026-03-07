3c9165abee8496e177e61f255cbda213c52b8c4e
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2026-03-07 12:30:01 +0100

    add test for script odfdo-flat

---
+++ tests/scripts/test_odfdo_flat.py
@@ -0,0 +1,177 @@
+# Copyright 2018-2026 Jérôme Dumonteil
+# Authors (odfdo project): jerome.dumonteil@gmail.com
+from __future__ import annotations
+
+import platform
+import subprocess
+import sys
+from pathlib import Path
+
+import pytest
+
+from odfdo import Document
+from odfdo.scripts import flat
+from odfdo.scripts.flat import main as main_script
+from odfdo.scripts.flat import main_convert_flat, parse_cli_args
+from odfdo.scripts.folder import main_convert_folder
+from odfdo.scripts.folder import parse_cli_args as folder_parse_cli_args
+
+SCRIPT = Path(flat.__file__)
+
+
+def run_params(params: list):
+    command = [sys.executable, SCRIPT] + params
+    proc = subprocess.Popen(
+        command,
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    out, err = proc.communicate()
+    return out, err, proc.returncode
+
+
+def test_flat_no_param():
+    params = []
+    out, err, exitcode = run_params(params)
+    print(out, err, exitcode)
+    assert exitcode == 2
+    assert "odfdo-flat: error: the following arguments are required" in err
+    assert "usage" in err
+
+
+# direct access to internal function
+
+
+def test_flat_2_no_param_on_main_function(monkeypatch):
+    with pytest.raises(SystemExit) as result:
+        monkeypatch.setattr(sys, "argv", [])
+        main_script()
+        assert result.value.code >= 1
+
+
+@pytest.mark.skipif(
+    platform.system() == "Windows",
+    reason="Fails on Windows due to stdin/output capture conflict.",
+)
+def test_flat_2_no_param():
+    with pytest.raises(SystemExit) as result:
+        params = parse_cli_args([])
+        main_convert_flat(params)
+        assert result.value.code >= 1
+
+
+def test_flat_2_version(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["--version"])
+        assert result.value.code == 0
+    captured = capsys.readouterr()
+
+    assert "odfdo-flat v3" in captured.out
+
+
+def test_flat_2_help(capsys):
+    with pytest.raises(SystemExit) as result:
+        parse_cli_args(["--help"])
+        assert result.value.code == 0
+    captured = capsys.readouterr()
+
+    assert "Convert a standard ODF file" in captured.out
+
+
+def test_flat_2_no_file_on_main(monkeypatch):
+    with pytest.raises(SystemExit) as result:
+        monkeypatch.setattr(sys, "argv", ["xxx", "none_file"])
+        main_script()
+        assert result.value.code >= 1
+
+
+def test_flat_2_no_file():
+    params = parse_cli_args(["none_file"])
+    with pytest.raises(SystemExit) as result:
+        main_convert_flat(params)
+        assert result.value.code >= 1
+
+
+def test_flat_2_odt_to_fodt(capsys, tmp_path, samples):
+    source = samples("test_diff1.odt")
+    document_src = Document(source)
+    tmp_source = tmp_path / "test.odt"
+    document_src.save(tmp_source)
+    assert tmp_source.exists()
+    params = parse_cli_args([str(tmp_source)])
+    main_convert_flat(params)
+    captured = capsys.readouterr()
+    print(captured.out)
+    print(captured.err)
+    path = tmp_path / "test.fodt"
+    assert path.is_file()
+
+
+def test_flat_2_fodt_to_odt(tmp_path, samples):
+    source = samples("test_diff1.odt")
+    document_src = Document(source)
+    tmp_source = tmp_path / "test.odt"
+    document_src.save(tmp_source)
+    # First convert to flat
+    params1 = parse_cli_args([str(tmp_source)])
+    main_convert_flat(params1)
+    tmp_source.unlink()
+    assert not tmp_source.exists()
+    # Then convert back to ODF
+    params2 = parse_cli_args([str(tmp_path / "test.fodt")])
+    main_convert_flat(params2)
+    assert tmp_source.exists()
+
+
+def test_flat_2_ods_to_fods(capsys, tmp_path, samples):
+    source = samples("simple_table.ods")
+    document_src = Document(source)
+    tmp_source = tmp_path / "test.ods"
+    document_src.save(tmp_source)
+    assert tmp_source.exists()
+    params = parse_cli_args([str(tmp_source)])
+    main_convert_flat(params)
+    captured = capsys.readouterr()
+    print(captured.out)
+    print(captured.err)
+    path = tmp_path / "test.fods"
+    assert path.is_file()
+
+
+def test_flat_2_fods_to_ods(tmp_path, samples):
+    source = samples("simple_table.ods")
+    document_src = Document(source)
+    tmp_source = tmp_path / "test.ods"
+    document_src.save(tmp_source)
+    # First convert to flat
+    params1 = parse_cli_args([str(tmp_source)])
+    main_convert_flat(params1)
+    tmp_source.unlink()
+    assert not tmp_source.exists()
+    # Then convert back to ODF
+    params2 = parse_cli_args([str(tmp_path / "test.fods")])
+    main_convert_flat(params2)
+    assert tmp_source.exists()
+
+
+def test_flat_2_folder_to_fodt(capsys, tmp_path, samples):
+    # First create a folder structure
+    source = samples("test_diff1.odt")
+    document_src = Document(source)
+    tmp_source = tmp_path / "test.odt"
+    document_src.save(tmp_source)
+    # Convert to folder
+
+    params1 = folder_parse_cli_args([str(tmp_source)])
+    main_convert_folder(params1)
+    tmp_source.unlink()
+    assert not tmp_source.exists()
+    # Convert folder to flat
+    params2 = parse_cli_args([str(tmp_path / "test.odt.folder")])
+    main_convert_flat(params2)
+    captured = capsys.readouterr()
+    print(captured.out)
+    print(captured.err)
+    path = tmp_path / "test.fodt"
+    assert path.is_file()
